17041a4a96f0ba8e1b61754e1efdf4432b01356a
CHAKIR MOHAMED <mchakir1974@gmail.com>
2022-11-06 11:18:13 +0100

    test

--- CLIENT/admin.py
+++ CLIENT/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+#TypePermis, Permis, Nationalite, Client, TypeAvenant, Produit,Vehicule, TypePaiement, Paiement, Contrat
+
+
+admin.site.register(TypePermis)
+admin.site.register(Permis)
+admin.site.register(Nationalite)
+admin.site.register(Client)
+admin.site.register(TypeAvenant)
+admin.site.register(Produit)
+admin.site.register(Vehicule)
+admin.site.register(TypePaiement)
+admin.site.register(Paiement)
+admin.site.register(Contrat)
